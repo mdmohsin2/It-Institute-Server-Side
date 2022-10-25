0584acb746d4cfbd0a641471db866ab1f8396afc
mdmohsin2 <mohsinbd862554@gmail.com>
2022-10-25 18:50:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const course = require('./data.json')
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+
+app.get('/course', (req, res) => {
+    res.send(course)
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
