bccf9a126095c1a5d9f55f996edc0febf6cf3e99
mdmohsin2 <mohsinbd862554@gmail.com>
2022-10-25 21:39:45 +0600

    category creat

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const cors = require('cors');
+app.use(cors());
 
+const category = require('./Categoris.json')
 const course = require('./data.json')
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
 
 
 app.get('/allcourse', (req, res) => {
-    res.send(course)
-  })
-  
-  app.listen(port, () => {
+    res.send(category)
+})
+
+
+app.get('/allcourse/:id', (req, res) => {
+    const id = req.params.id;
+    const selectCourse = course.find(c => c.id == id)
+    res.send(selectCourse)
+})
+
+
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
